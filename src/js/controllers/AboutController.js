216b148dbe48b769ae7337e3bfc01a954909ffd7
Samantha Davis <samantha.shea.davis@gmail.com>
2016-12-08 11:29:35 -0500

    layout, style and js for tabs about me page

--- src/js/controllers/AboutController.js
+++ src/js/controllers/AboutController.js
@@ -1,3 +1,28 @@
 angular.module('portfolioApp').controller('AboutController', function($state) {
-  this.message = "in AboutController";
+
+// set the tabs on page load to show about me first.
+
+this.showAbout = true;
+this.showSkills = false;
+this.showInterests = false;
+
+
+// tabswitch handles the click action on the tabs, adding the active class to the selected tab and showing the correct page
+
+  this.tabSwitch = function(tab) {
+    if (tab === 'about') {
+      this.showAbout = true;
+      this.showSkills = false;
+      this.showInterests = false;
+    } else if (tab === 'skills') {
+      this.showSkills = true;
+      this.showAbout = false;
+      this.showInterests = false;
+    } else if (tab === 'interests') {
+      this.showInterests = true;
+      this.showAbout = false;
+      this.showSkills = false;
+    }
+
+  };
 });
