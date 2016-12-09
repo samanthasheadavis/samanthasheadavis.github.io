b27f47649d912351bfd7a160b4ab7cf83fda9cff
Samantha Davis <samantha.shea.davis@gmail.com>
2016-12-09 12:19:29 -0500

    styled projects page

--- src/js/controllers/ProjectsController.js
+++ src/js/controllers/ProjectsController.js
@@ -1,10 +1,11 @@
-angular.module('portfolioApp').controller('ProjectsController', function($state, RequestService) {
+angular.module('portfolioApp').controller('ProjectsController', function($state, RequestService, $scope) {
   this.message = "in ProjectsController";
 
   this.getProjects = function() {
-    console.log('in');
     RequestService.getProjects(function(data) {
-      console.log(data.projects[0].date);
+      $scope.projects = data.projects;
+      console.log($scope.projects);
+      $scope.$apply();
     });
   };
 
