4730009dcf7e8c370634a26823f1dffb53f5e413
Samantha Davis <samantha.shea.davis@gmail.com>
2016-12-12 13:59:12 -0500

    added ng-focus to input items so user can tab through form

--- src/js/controllers/ContactController.js
+++ src/js/controllers/ContactController.js
@@ -1,6 +1,6 @@
 angular.module('portfolioApp').controller('ContactController', function($state) {
-    this.targetItem = function(element) {
 
+    this.targetItem = function(element) {
         $('.' + element).css({
             'top': '5px',
             'left': '0px'
